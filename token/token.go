2a1fb312842c02b5eded20b2218ae444e8615d6d
pandasoli <sadpandasoli@gmail.com>
2023-01-21 01:17:03 -0300

    update

--- token/token.go
+++ token/token.go
@@ -30,3 +30,16 @@ type Token struct {
   Literal string
 }
 
+var keywords = map[string] TokenType {
+  "fn": FUNCTION,
+  "let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+  if tok, ok := keywords[ident]; ok {
+    return tok
+  }
+
+  return IDENT
+}
+
